5bce2795b635eb55324ba4631f930b52fe05a7bc
Abzal Haırýllauly <abzalzhumabaev@gmail.com>
2023-01-25 12:01:57 +0600

    initial commit

---
+++ src/components/stencil-generated/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@az-way.com/dls-stencil';
+
+import { defineCustomElements } from '@az-way.com/dls-stencil/loader';
+
+defineCustomElements();
+export const DlsCard = /*@__PURE__*/createReactComponent<JSX.DlsCard, HTMLDlsCardElement>('dls-card');
+export const DlsLinks = /*@__PURE__*/createReactComponent<JSX.DlsLinks, HTMLDlsLinksElement>('dls-links');
+export const DlsOgSection = /*@__PURE__*/createReactComponent<JSX.DlsOgSection, HTMLDlsOgSectionElement>('dls-og-section');
